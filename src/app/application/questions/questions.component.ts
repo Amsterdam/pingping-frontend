337663899aeef3cbe2d7637e17a7c21d1a68c4a7
Carlos Castillo <cmaxhm@yahoo.com>
2019-08-15 16:51:17 -0500

    - Questions template - Questions navigation base

--- src/app/application/questions/questions.component.ts
+++ src/app/application/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,22 +8,40 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class QuestionsComponent implements OnInit {
   questionsForm: FormGroup;
-
+  questionsNumber: number = 6;
+  questionPosition = 1;
+  progressPercentage: number;
   years: number[] = [];
 
   constructor() {
+    this.progressPercentage = 0;
     this.getYearsArray();
-
     this.questionsForm = new FormGroup({
       'question1': new FormGroup({
         'birthDay': new FormControl(''),
         'birthMonth': new FormControl(''),
         'birthYear': new FormControl('')
+      }),
+      'question2': new FormGroup({
+        'answer2': new FormControl('')
+      }),
+      'question3': new FormGroup({
+        'answer3': new FormControl('')
+      }),
+      'question4': new FormGroup({
+        'answer4': new FormControl('')
+      }),
+      'question5': new FormGroup({
+        'answer5': new FormControl('')
+      }),
+      'question6': new FormGroup({
+        'answer6': new FormControl('')
       })
     });
   }
 
   ngOnInit() {
+    this.updateProgressBar();
   }
 
   getYearsArray() {
@@ -35,7 +53,27 @@ export class QuestionsComponent implements OnInit {
     }
   }
 
+  previousQuestion() {
+    if ((this.questionPosition - 1) > 0) {
+      this.questionPosition -= 1;
+
+      this.updateProgressBar();
+    }
+  }
+
   nextQuestion() {
-    console.log(this.questionsForm);
+    if ((this.questionPosition + 1) <= this.questionsNumber) {
+      this.questionPosition += 1;
+
+      this.updateProgressBar();
+    }
+  }
+
+  updateProgressBar() {
+    this.progressPercentage = (100 * this.questionPosition) / this.questionsNumber;
+  }
+
+  finishQuestionnaire() {
+    console.log(this.questionsForm.value);
   }
 }
