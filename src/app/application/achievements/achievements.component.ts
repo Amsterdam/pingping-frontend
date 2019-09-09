fe8bc4b85e4b9e749b0442d93f87c6adcead730d
Carlos Castillo <cmaxhm@yahoo.com>
2019-09-09 11:51:32 -0500

    Achievements data from backend

--- src/app/application/achievements/achievements.component.ts
+++ src/app/application/achievements/achievements.component.ts
@@ -8,12 +8,19 @@ import { AppService } from '../../services/app.service';
 })
 export class AchievementsComponent implements OnInit {
   user: any = {};
+  achievements: any = {};
+  userAchievements: any = {};
+  achievementsEarned: any = [];
 
   constructor(private appService: AppService) { }
 
   ngOnInit() {
     this.appService.getUser().subscribe(response => {
       this.user = response;
+    });
+
+    this.appService.getAchievements().subscribe(response => {
+      this.achievements = response;
     });
   }
 }
