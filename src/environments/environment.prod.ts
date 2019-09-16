f1e0dd299eab95734743739751872acc4224fbb1
Mani Gudvardarson <mani@Manis-MacBook-Pro-2.local>
2019-09-16 18:37:37 +0200

    Cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.pingping.amsterdam/api',
-  matomoUrl: 'https://analytics.data.amsterdam.nl/matomo.js',
+  matomoUrl: 'https://analytics.data.amsterdam.nl/',
   matomoSiteId: 30
 };
