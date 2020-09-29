15f26e9132ac69be76714685acbcdae733d50745
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2020-09-29 12:02:04 +0200

    Icons file removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugins/icons.js'],
+  plugins: [],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
